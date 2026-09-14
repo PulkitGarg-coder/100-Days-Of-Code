88882643abd8f20312eee81d74504becad5e201f
PulkitGarg-coder <pulkitgkrm@gmail.com>
2026-09-14 23:06:46 +0530

    Day 3

---
+++ Swap using a third variable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, temp;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nBefore swapping:");
+    printf("\na = %d", a);
+    printf("\nb = %d", b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("\n\nAfter swapping:");
+    printf("\na = %d", a);
+    printf("\nb = %d", b);
+
+    return 0;
+}
